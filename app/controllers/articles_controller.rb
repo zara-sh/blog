8d5ca24eff5206f91fc03851fbef5db4ce49632e
zara-sh <zara.regist@gmail.com>
2018-04-04 23:25:58 +0900

    adding show and index action  to articles with their view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,25 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspec
+    @article = Article.new(article_parmas)
+    @article.save
+    redirect_to @article
+  end
+
+  private
+  def article_parmas
+    params.require(:article).permit(:title, :text)
   end
 end
